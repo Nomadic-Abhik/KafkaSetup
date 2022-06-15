72b7fb9a2d5f5bc8faad0bb9b6005f6fb9dafa11
Abhik Mitra <abhik.mitra06@gmail.com>
2022-06-15 15:00:31 +0530

    Add project files.

---
+++ ConsumerApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace ConsumerApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "weather-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+            consumer.Subscribe("Weather-topic");
+            CancellationTokenSource token = new();
+            try
+            {
+                while(true)
+                {
+                    var response = consumer.Consume(token.Token);
+                    if(response.Message != null)
+                    {
+                        var weather = JsonConvert.DeserializeObject<WeatherForecast>(response.Message.Value);
+                        Console.WriteLine(weather);
+                    }
+                }
+                return null;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ProducerApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Newtonsoft.Json;
+
+namespace ProducerApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            WeatherForecast model = new WeatherForecast
+            {
+                Date = DateTime.Now,
+                TemperatureC = 32,
+                Summary = "This is today Weather"
+            };
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+            try
+            {
+                while (model!= null)
+                {
+                    var response = await producer.ProduceAsync("Weather-topic",
+                        new Message<Null, string> { Value = JsonConvert.SerializeObject(model)});
+                }
+                
+                return null;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
